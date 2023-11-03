ed5f423ecb89322b1abc39d6bf09dee446a3a149
naonoob <131419047+naonoob@users.noreply.github.com>
2023-11-03 08:23:45 +0000

    授業進行

--- app6.js
+++ app6.js
@@ -7,6 +7,24 @@ const db = new sqlite3.Database('test.db');
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/insert", (req, res) => {
+  let sql = `insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);`
+console.log(sql);
+  db.serialize( () => {
+    db.run( sql, (error, row) => {
+     console.log(error);
+      if(error) {
+        res.render('show', {mes:"エラーです"});
+       }
+     res.render('show', {mes:"成功しました"})
+    });
+  });
+  console.log(req.body);
+});
+
 app.get("/", (req, res) => {
   const message = "Hello world";
   res.render('show', {mes:message});
@@ -38,8 +56,24 @@ app.get("/top", (req, res) => {
         })
     })
 })
+
+
+
+app.get("/db/:id",(req,res)=>{
+  console.log(req.params.id);
+  db.serialize(()=>{
+    db.all("select id,都道府県,人口,大学 from example where id=" +req.params.id +";", (error,row)=>{
+      if(error){
+        res.render('show',{mes:"エラーです"});
+      }
+      res.render('db',{data:row});
+    });
+  })
+})
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
+
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
